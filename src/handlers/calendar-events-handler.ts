f58915ed41c7503f2048d8306ae02a057a720c21
Yuma Ito <38903000+yuma-ito-bd@users.noreply.github.com>
2025-01-05 19:50:49 +0900

    Lambdaが最後まで実行されるようにawait

--- src/handlers/calendar-events-handler.ts
+++ src/handlers/calendar-events-handler.ts
@@ -2,6 +2,7 @@ import { CalendarEventsNotifier } from "../calendar-events-notifier";
 import { GoogleCalendarApiAdapter } from "../google-calendar-api-adapter";
 
 export const calendarEventsHandler = async () => {
+  console.info("Start calendar events handler");
   const googleCalendarApi = new GoogleCalendarApiAdapter();
-  new CalendarEventsNotifier(googleCalendarApi).call();
+  await new CalendarEventsNotifier(googleCalendarApi).call();
 };
